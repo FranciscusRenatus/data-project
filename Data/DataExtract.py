ce8006824644a39a1f3206eff8bc6f2e65d650c1
Karst Backer <karst@makajelaan.nl>
2018-06-07 17:17:15 +0200

    cleanup of outdated/redundant files

--- Data/DataExtract.py
+++
@@ -1,9 +0,0 @@
-import urllib.request, json 
-
-def requestData(link):
-    with urllib.request.urlopen(link) as url:
-        data = json.loads(url.read().decode())
-        return data
-
-if __name__ == "__main__":
-    print(requestData("http://opendata.cbs.nl/ODataApi/OData/81975NED/UntypedDataSet"))
